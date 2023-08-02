70d81a6d05e68329ef6d774ac435664b0e404763
Jrorem <joerorem@gmail.com>
2023-08-02 18:52:21 -0500

    updated app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Resume from './pages/Resume'
+import './components/styles.css'
+import { Router, Route, Routes } from 'react-router-dom'
+import background from "../src/assets/background.jpg";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router basename='react-portfolio'>
+    <div class="App" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', height: '100vh'}}>  
+    <Header/>  
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/resume' element={<Resume/>} />
+      </Routes>
+      <Footer/>
+      
     </div>
+    </Router>
   );
 }
+
+
 
 export default App;
