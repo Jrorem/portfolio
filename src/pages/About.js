a37c77b7e12c76d55890a360e5aa1db1044bf2ed
Jrorem <joerorem@gmail.com>
2023-08-02 21:02:02 -0500

    404 about fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,5 +20,17 @@ export default function About () {
         <h1>My name is Joe Rorem and I am a junior developer. I have a background in welding and fabrication, with some commercial fishing experience.
             I am looking to change careers and be a programmer.
         </h1>
+       
+      {function(l) {
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) { 
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location)}
+    
     </div> 
 }
